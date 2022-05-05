0b1e5c48e24fcfa99e5948f67c4ca8860f5509db
carlos chaguendo ml <carlos.chaguendo@mercadolibre.com.co>
2022-05-05 17:40:40 -0500

    Scrollable at top

---
+++ TestView/AndesModal/View/Pages/Variations/AndesModalPageAbstractView.swift
@@ -0,0 +1,14 @@
+//
+//  AndesModalPageAbstractView.swift
+//  TestView
+//
+//  Created by Carlos Ml on 5/05/22.
+//
+
+import UIKit
+
+@IBDesignable class AndesModalPageAbstractView: UIViewXIB {
+
+   
+
+}
---
+++ TestView/AndesModal/View/Pages/Variations/AndesModalPageViewDefault.swift
@@ -0,0 +1,12 @@
+//
+//  AndesModalPageView.swift
+//  TestView
+//
+//  Created by Carlos Ml on 5/05/22.
+//
+
+import UIKit
+
+class AndesModalPageViewDefault: UIViewXIB {
+
+}
